f27ba947427b16a136734f3fd1368b546db5a82e
Zhang Jin <zhangjin11@baidu.com>
2019-07-22 10:12:15 +0800

    Longest Common Prefix

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ leetcode in Java  解题笔记
 - Reverse Integer 数字翻转
 - Palindrome Number 回文数
 - Roman To Integer 罗马数字转阿拉伯数字
+- Longest Common Prefix 最长公共前缀
---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,25 @@
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        int size = strs.length;
+        if (size == 0) {
+            return "";
+        }
+        String minStr = "";
+        int len = strs[0].length();
+        for (String str : strs) {
+            if (str.length() <= len) {
+                len = str.length();
+                minStr = str;
+            }
+        }
+        int i = 0;
+        for (; i < len; i++) {
+            for (String str : strs) {
+                if (str.charAt(i) != minStr.charAt(i)) {
+                    return minStr.substring(0, i);
+                }
+            }
+        }
+        return minStr;
+    }
+}
