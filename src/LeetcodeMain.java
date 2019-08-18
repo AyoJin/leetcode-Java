7532f350f2a44b359e0727f4d7de748c26941def
zhangjin <zhangjinayojin@gmail.com>
2019-08-18 23:19:38 +0800

    Valid Parentheses

--- src/LeetcodeMain.java
+++
@@ -1,5 +0,0 @@
-public class LeetcodeMain {
-    public static void main(String[] args) {
-        ReverseInteger.reverse(-9554);
-    }
-}
---
+++ src/java/ValidParentheses.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+    private Stack<Character> stack = new Stack<>();
+
+    private Map<Character, Character> map;
+
+    public ValidParentheses() {
+        map = new HashMap<>();
+        map.put('}', '{');
+        map.put(')', '(');
+        map.put(']', '[');
+    }
+
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (map.containsKey(ch)) {
+                char top = stack.empty() ? '#' : stack.pop();
+                if (top != map.get(ch)) {
+                    return false;
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
