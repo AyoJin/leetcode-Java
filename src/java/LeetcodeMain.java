97822ebb16d8e0412f4547f6c8d82a22f842cfce
Zhang Jin <zhangjin11@baidu.com>
2019-08-19 14:57:13 +0800

    Merge Two Sorted Lists

--- src/java/LeetcodeMain.java
+++ src/java/LeetcodeMain.java
@@ -1,11 +1,6 @@
 public class LeetcodeMain {
     public static void main(String[] args) {
-        ValidParentheses vp = new ValidParentheses();
-        if (vp.isValid("{()[}]")) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
+        return;
     }
 
 }
