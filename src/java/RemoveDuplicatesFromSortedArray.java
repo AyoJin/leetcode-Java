0a9c5e4349577135d050524de80566deb3bfe879
zhangjin <zhangjinayojin@gmail.com>
2019-08-21 01:36:27 +0800

    Remove Duplicates From Sorted Array

---
+++ src/java/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,27 @@
+public class RemoveDuplicatesFromSortedArray {
+
+    public int removeDuplicates(int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return 0;
+        }
+        int res = 1;
+        for (int i = 0; i < len; i++) {
+            int j = i + 1;
+            while (j < len) {
+                if (nums[i] != nums[j]) {
+                    nums[++i] = nums[j];
+                    res++;
+                } else {
+                    j++;
+                }
+                if (j == len) {
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+
+
+}
