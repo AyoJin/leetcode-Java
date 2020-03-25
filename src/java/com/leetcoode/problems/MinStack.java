332b7ee43ab34653c294d7682cec290970b154cf
zhangjin11 <zhangjin11@baidu.com>
2020-03-25 16:50:09 +0800

    Min Stack

--- src/java/com/leetcoode/problems/MinStack.java
+++ src/java/com/leetcoode/problems/MinStack.java
@@ -3,41 +3,45 @@ package com.leetcoode.problems;
 import java.util.Stack;
 
 public class MinStack {
-    /** initialize your data structure here. */
-    private Stack<Integer> stack1;
-    private Stack<Integer> stack2;
-    private int min = Integer.MIN_VALUE;;
+    /**
+     * initialize your data structure here.
+     */
+    private Stack<Integer> data;
+    private Stack<Integer> helper;
 
     public MinStack() {
-        stack1 = new Stack<>();
-        stack2 = new Stack<>();
+        data = new Stack<>();
+        helper = new Stack<>();
     }
 
     public void push(int x) {
-        stack1.push(x);
-        min = min < x ? min : x;
+        data.push(x);
+        if (helper.isEmpty() || x <= helper.peek()) { // 等于也要入栈！
+            helper.push(x);
+        }
     }
 
     public void pop() {
-        stack1.pop();
-        if (stack1.isEmpty()) {
-            return;
-        }
-        min = stack1.peek();
-        while (!stack1.isEmpty()) {
-            min = min < stack1.peek() ? min : stack1.peek();
-            stack2.push(stack1.pop());
-        }
-        while (!stack2.isEmpty()) {
-            stack1.push(stack2.pop());
+        if (!data.isEmpty()) {
+            // Integer比较为什么不能用 == ？
+            if (data.peek().equals(helper.peek())) {
+                helper.pop();
+            }
+            data.pop();
         }
     }
 
     public int top() {
-        return stack1.peek();
+        if (data.isEmpty()) {
+            throw new RuntimeException("StackIsEmpty!");
+        }
+        return data.peek();
     }
 
     public int getMin() {
-        return min;
+        if (helper.isEmpty()) {
+            throw new RuntimeException("StackIsEmpty!");
+        }
+        return helper.peek();
     }
 }
